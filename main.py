4ed4a26d2849072c0de46ea7da66e3e55c71de39
Olivercarbery <olivercarbery3@gmail.com>
2025-08-12 13:07:14 +0100

    done

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
 from ryanair import Ryanair
-from datetime import datetime
+from datetime import timedelta
 from datetime import date
 
 api = Ryanair("GBP")
 
-# Departure airport and date frame
-Airport = "MAN"
+# Departure airport and departure date frame
+Airport1 = "MAN"
+Airport2 = "LPL"
+
 startDate = date(2025, 8, 12)
 endDate = date(2025, 12, 30)
 max_price = 150
+trip_length = 7
 
-flights = api.get_cheapest_flights(Airport, startDate, endDate)
+flights = api.get_cheapest_flights(Airport1, startDate, endDate)
 
+# loop through all the flights found and filter for price and not in the UK
 for i in range(len(flights)):
     if flights[i].price < max_price and flights[i].destinationFull:
-        print(flights[i].destinationFull)
-        print(flights[i].price)
+        print(f'{Airport1} to {flights[i].destinationFull}')
+        print(f'Outbound Flight Price: £{flights[i].price}')
+        print(f'Outbound Flight Date: {flights[i].departureTime}')
 
+        # Use trip length to find cheap return flights
+        returnFlight = api.get_cheapest_flights(flights[i].destination, flights[i].departureTime+timedelta(days=trip_length), flights[i].departureTime + timedelta(days=trip_length))
+        for j in range(len(returnFlight)):
+            if returnFlight[j].destination == Airport1:
+                print(f'Return Flight {Airport1} Price: £{returnFlight[j].price}')
+                print(f'Return Flight {Airport1} Time: {returnFlight[j].departureTime}')
+                print(f'{Airport1} Total Price: £{flights[i].price+returnFlight[j].price}')
+
+            if returnFlight[j].destination == Airport2:
+                print(f'Return Flight')
+    print('\n')
 
 
