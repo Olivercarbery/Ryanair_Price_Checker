b665f8372154104aedb07a2c4cd66f70562c501e
Olivercarbery <olivercarbery3@gmail.com>
2025-08-12 13:44:40 +0100

    Finished code

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ flights = api.get_cheapest_flights(Airport1, startDate, endDate)
 
 # loop through all the flights found and filter for price and not in the UK
 for i in range(len(flights)):
-    if flights[i].price < max_price and flights[i].destinationFull:
+    if flights[i].price < max_price and "United Kingdom" not in flights[i].destinationFull:
+        # Print flight data to console
         print(f'{Airport1} to {flights[i].destinationFull}')
         print(f'Outbound Flight Price: £{flights[i].price}')
         print(f'Outbound Flight Date: {flights[i].departureTime}')
@@ -26,12 +27,36 @@ for i in range(len(flights)):
         returnFlight = api.get_cheapest_flights(flights[i].destination, flights[i].departureTime+timedelta(days=trip_length), flights[i].departureTime + timedelta(days=trip_length))
         for j in range(len(returnFlight)):
             if returnFlight[j].destination == Airport1:
-                print(f'Return Flight {Airport1} Price: £{returnFlight[j].price}')
-                print(f'Return Flight {Airport1} Time: {returnFlight[j].departureTime}')
+                # Print return flight data
+                print(f'Return to {Airport1} Flight Price: £{returnFlight[j].price}')
+                print(f'Return to {Airport1} Flight Time: {returnFlight[j].departureTime}')
                 print(f'{Airport1} Total Price: £{flights[i].price+returnFlight[j].price}')
 
             if returnFlight[j].destination == Airport2:
-                print(f'Return Flight')
+                print(f'Return to {Airport2} Flight Price: £{returnFlight[j].price}')
+                print(f'Return to {Airport2} Flight Date: {returnFlight[j].departureTime}')
+                print(f'{Airport2} Total Price: £{flights[i].price+returnFlight[j].price}')
+
+    print('\n'*2)
+print('\n'*5)
+print(f'~~~~~~~ Flights from {Airport2} ~~~~~~~')
+flights = api.get_cheapest_flights(Airport2, startDate, endDate)
+for i in range(len(flights)):
+    if flights[i].price < max_price and "United Kingdom" not in flights[i].destinationFull:
+        print(f'{Airport2} to {flights[i].destinationFull}')
+        print(f'Outbound Flight Price: £{flights[i].price}')
+        print(f'Outbound Flight Time: {flights[i].departureTime}')
+
+        returnFlight = api.get_cheapest_flights(flights[i].destination, flights[i].departureTime + timedelta(days= trip_length), flights[i].departureTime + timedelta(days=trip_length))
+
+        for j in range(len(returnFlight)):
+            if returnFlight[j].destination == Airport2:
+                print(f'Return to {Airport2} Flight Price: £{returnFlight[j].price}')
+                print(f'Return to {Airport2} Flight Time: {returnFlight[j].departureTime}')
+                print(f'{Airport2} Total Price: £{flights[i].price+returnFlight[j].price}')
+
+            if returnFlight[j].destination == Airport1:
+                print(f'Return to {Airport1} Flight Price: £{returnFlight[j].price}')
+                print(f'Return to {Airport1} Flight Time: {returnFlight[j].departureTime}')
+                print(f'{Airport1} Total Price: £{flights[i].price+returnFlight[j].price}')
     print('\n')
-
-
